1adc7dab05c71b7bff3320ee01952a55fdb74fd2
nimsaraanuhas <nimsaraanuhas35@gmail.com>
2024-12-26 00:04:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "dlQVwRiQ#UIIpRWL-RDCjbwKJLrDjb04evTj0yLEI9hkdeten17c",
+SESSION_ID: process.env.SESSION_ID || "4pRSmDhK#trXCYK9Cb1PwwMnZfbyyvC9mglHW6Dmlhs1p-ZT39NQ",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 MODE: process.env.MODE || "private",
 };
