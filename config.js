08417085cdcda546a86f1a2e668d19a604da0075
nimsaraanuhas <nimsaraanuhas35@gmail.com>
2024-12-26 00:12:02 -0800

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "4pRSmDhK#trXCYK9Cb1PwwMnZfbyyvC9mglHW6Dmlhs1p-ZT39NQ",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 MODE: process.env.MODE || "private",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/otfl6o.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM ANGEL MD IS AVAILABLE FOR YOUR HELP",
+BOT_NAME: process.env.BOT_NAME || "ANGEL  MD"
 };
