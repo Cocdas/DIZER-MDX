689e67f13e702cd8e14ed74e2e3603d8af41abda
nimsaraanuhas <nimsaraanuhas35@gmail.com>
2024-12-26 00:09:22 -0800

    Add files via upload

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,42 +1,20 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const os = require("os")
-
 cmd({
     pattern: "alive",
-    react: "👋",
+    react: "🌐",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let totalStorage = Math.floor(os.totalmem() / 1024 / 1024) + 'MB'
-let freeStorage = Math.floor(os.freemem() / 1024 / 1024) + 'MB'
-let desc = `👋 Hey ${pushname}, I Aᴍ Aʟɪᴠᴇ Nᴏᴡ
-
-ɪ ᴀᴍ ᴀɴ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ ꜱʏꜱᴛᴇᴍ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴛʜᴀᴛ ᴄᴀɴ ʜᴇʟᴘ ᴛᴏ ᴅᴏ ꜱᴏᴍᴇᴛʜɪɴɢ,ꜱᴇᴀʀᴄʜ ᴀɴᴅ ɢᴇᴛ ᴅᴀᴛᴀ / ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ᴏɴʟʏ ᴛʜᴏᴜɢʜ ᴡʜᴀᴛꜱᴀᴘᴘ
-
-> ʙᴏᴛ ɴᴀᴍᴇ : ᴅᴇɴᴇᴛʜ-ᴍᴅ ᴠ1
-> ᴛᴏᴛᴀʟ ʀᴀᴍ : ${totalStorage}
-> ꜰʀᴇᴇ ʀᴀᴍ : ${freeStorage}
-> ᴏᴡɴᴇʀ : ᴅᴇɴᴇᴛʜ-xᴅ
-
-🥰 𝗛𝗮𝘃𝗲 𝗮 𝗡𝗶𝗰𝗲 𝗗𝗮𝘆 🥰
-
-> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇɴᴇᴛʜ-xᴅ ᴛᴇᴄʜ®`
-    
-const sentMsg = await conn.sendMessage(from, {
-            image: { url: `https://github.com/Deneth400/DENETH-MD-HARD/blob/main/Images/DENETH-MD.jpg?raw=true`},  // Add the image URL here
-            caption: desc,  // Send the description as the caption
-            contextInfo: {
-                forwardingScore: 999,
-                isForwarded: true,
-            }
-        }, { quoted: mek });
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
+
+
 
