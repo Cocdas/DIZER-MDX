bfcfe2ee95585a293d9a32864dda6fde82c56929
DIZER03 <cmd45659@gmail.com>
2024-12-29 23:17:19 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,16 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
+
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "V54wgRaL#QJg8CqP1Nx5ixBz3wnDu5b7qC_75mK3SdZ4LZ0433FQ",
-AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
-MODE: process.env.MODE || "private",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/otfl6o.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM ANGEL MD IS AVAILABLE FOR YOUR HELP",
-BOT_NAME: process.env.BOT_NAME || "ANGEL  MD"
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true", 
+ 
 };
