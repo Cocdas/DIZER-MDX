615d001c6380d385b48e3537cd112b86e49af62a
CEYLON FUCKER <138351304+Cocdas@users.noreply.github.com>
2025-04-14 07:44:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "V54wgRaL#QJg8CqP1Nx5ixBz3wnDu5b7qC_75mK3SdZ4LZ0433FQ",
+SESSION_ID: process.env.SESSION_ID || "KExHETQL#QvJLxOQcoZfM6JySG-gjP6y3K59el3u97cEF2qM7i1k",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/otfl6o.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM ANGEL MD IS AVAILABLE FOR YOUR HELP",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
