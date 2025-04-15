d8f5a09e062c6667ed69e1a813329f1a2e8c1701
CEYLON FUCKER <138351304+Cocdas@users.noreply.github.com>
2025-04-14 20:22:52 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "KExHETQL#QvJLxOQcoZfM6JySG-gjP6y3K59el3u97cEF2qM7i1k",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rnwhal.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM DIZER MD IS AVAILABLE FOR YOUR HELP",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-BLOCK_JID: process.env.BLOCK_JID || "", //block jids    
- 
+STATUS_VIEW: process.env.STATUS_VIEW === undefined ? 'true' : process.env.STATUS_VIEW,
+ALWAYS_ONLINE: process.env.ADDRESSES === undefined ? 'true' : process.env.ADDRESSES,
+AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
+AUTO_RECORDING: process.env.AUTO_RECORDING === undefined ? 'true' : process.env.AUTO_RECORDING,
 };
